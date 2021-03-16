c01ef591b0f63cea1082bb44f2281d2d6592253b
Taras <seman6745@gmail.com>
2021-03-16 16:54:28 +0200

    fetch

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,53 @@
 'use strict';
+const ListURL ='https://mate-academy.github.io/phone-catalogue-static/api/phones.json';// eslint-disable-line
+const DetailsURL = 'https://mate-academy.github.io/phone-catalogue-static/api/phones/';// eslint-disable-line
 
-// write your code here
+const getPhones = (url) => {
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        setTimeout(() => {
+          errorNotification();
+        }, 5000);
+      }
+
+      return response.json();
+    });
+};
+
+function errorNotification() {
+  const notification = document.createElement('div');
+
+  notification.className = 'error-notification';
+  notification.innerText = 'ERROR';
+  document.body.append(notification);
+  console.warn('Error');// eslint-disable-line
+};
+
+function appendList(listOfPhones) {
+  const list = document.createElement('ul');
+
+  listOfPhones.forEach(phone => {
+    const li = document.createElement('li');
+
+    li.innerText = phone.name;
+    list.append(li);
+  });
+
+  document.body.append(list);
+
+  return listOfPhones;
+}
+
+function getPhoneDetails(listOfPhones) {
+  const newList = listOfPhones.map(phone => phone.id).map(id => {
+    fetch(`${DetailsURL}${id}.json`);
+  });
+
+  return Promise.all(newList);
+}
+
+getPhones(ListURL)
+  .then(appendList)
+  .then(getPhoneDetails)
+  .catch(errorNotification);
